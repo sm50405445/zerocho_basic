7bf293cd470526ea3b212a5c871a1eb0cf37e892
sd50712321 <sd50712321@naver.com>
2020-03-17 00:13:52 +0900

    rest API 서버구동

--- server1.js
+++ server1.js
@@ -14,6 +14,10 @@ const parseCookie = (cookie = '') => {
         },{})
 }
 
+const session = {
+
+}
+
 const server = http.createServer((req,res)=>{
     const cookies = parseCookie(req.headers.cookie)
     console.log(cookies)
